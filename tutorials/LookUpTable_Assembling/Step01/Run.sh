09137e17cc22b66d5dd2f72470021e8a4d68fa8f
Bruno Santos <bruno.santos@bluecape.com.pt>
2012-04-15 12:22:37 +0100

    Moved LookUpTable_Assembling and minor tweak to Step01/Run.sh

--- tutorials/LookUpTable_Assembling/Step01/Run.sh
+++ tutorials/LookUpTable_Assembling/Step01/Run.sh
@@ -1,1 +1,2 @@
 ../../../utilities/exe/OpenSMOKE_SteadyStateFlamelet.sh --kinetics "../../../utilities/kinetics/PreProcessed/PolimiH2_1006" --input "Data.inp" --schedule "Operations.inp"  
+ln -s Flamelet_PolimiH2_1006/Profiles Flamelet-0
